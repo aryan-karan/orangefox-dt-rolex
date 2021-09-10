bde1a3015726bac6e6c82d0aba955821cc4470f2
Aryan Karan <aryankaran28022004@gmail.com>
2021-09-10 23:17:09 +0530

    OrangeFox Variables added

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,25 @@
 # limitations under the License.
 #
 
-add_lunch_combo omni_rolex-userdebug
-add_lunch_combo omni_rolex-eng
+for flavour in eng userdebug; do
+ add_lunch_combo omni_rolex-$flavour
+done
+
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export OF_USE_TWRP_SAR_DETECT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_QUICK_BACKUP_LIST="/data;/boot;/system_root;"
+
+export FOX_VERSION="R11.1"
+export FOX_BUILD_TYPE="Stable"
+export FOX_DEVICE_MODEL="Xiaomi Redmi 4A"
+export OF_MAINTAINER_AVATAR=""
+
+export OF_PATCH_AVB20=1
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_XZ_UTILS=1
